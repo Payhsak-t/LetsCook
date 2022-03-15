4a54626431c1a465f360c94068ca973c0e2030e9
Payhsak-t <triparnawinx@gmail.com>
2022-03-15 23:48:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const searchMeal = document.getElementById('search');
+const searchMealBtn = document.getElementById('search-btn');
+const randomMealBtn = document.getElementById('random-btn');
+const meal = document.getElementById('meal');
