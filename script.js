d7582e32acf70a6d275b39eb4ea7084ecf2f54f3
Payhsak-t <triparnawinx@gmail.com>
2022-03-22 01:33:48 +0530

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
 'use strict';
 
-const searchMeal = document.getElementById('search');
-const searchMealBtn = document.getElementById('search-btn');
-const randomMealBtn = document.getElementById('random-btn');
-const meal = document.getElementById('meal');
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const random = document.getElementById('random');
+const mealsEl = document.getElementById('meals');
+const resultHeading = document.getElementById('result-heading');
+const singleMealEl = document.getElementById('single-meal');
+
+//function to search meal
+function searchMeal(e) {
+  e.preventDefault();
+
+  //Clear single meal
+  singleMealEl.innerHTML = '';
+
+  //Get search Term
+  const term = search.value;
+
+  //check for empty
+  if (term.trim()) {
+    //Fetch Data from API
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2 class="result-heading">Search results for "${term}":</h2>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results. Please try again.</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              item =>
+                `<div class="meal">
+              <img src="${item.strMealThumb}" alt="${item.strMeal}" />
+              <div class="meal-info" data-mealID ="${item.idMeal}">
+                <h3>${item.strMeal}</h3>
+              </div>
+            </div>`
+            )
+            .join('');
+        }
+      });
+    //clear search value
+    search.value = '';
+  } else {
+    alert('Please enter a search term');
+  }
+}
+
+//Event Listener for search
+submit.addEventListener('submit', searchMeal);
